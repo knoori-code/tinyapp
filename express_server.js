177e75395c8a9a797505f5b2305b2735e59a2316
knoori-code <knoori3@gmail.com>
2023-01-23 19:40:35 -0500

    Add views folder and created a route for '/urls'

--- express_server.js
+++ express_server.js
@@ -9,9 +9,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
